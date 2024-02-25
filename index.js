c89f98355209eae82325d6f8ce7a5edae0db415a
wly <1059387852@qq.com>
2024-02-25 19:20:12 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var first = true;
 document.onkeydown = function (event) {
     var e = event || window.event || arguments.callee.caller.arguments[0];
-    if (e && e.keyCode == 13) { // 按 Esc 
+    if (e && e.keyCode == 13) { // 按 Enter 
         //开始游戏
         startGame();
         if (first) {
